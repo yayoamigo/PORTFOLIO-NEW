3f902eb4ae2ce896da1d20eef37a33968d09b8b7
david zambrano corral <Yayoamigo>
2023-01-28 18:27:21 -0500

    Add dots

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import { useState } from "react";
+import Navbar from "./scenes/Navbar";
+import DotGroup from "./scenes/DotGroup";
 import useMediaQuery from "./hooks/useMediaQuery";
-import Navbar from "./components/Navbar";
-
+import { useEffect, useState } from "react";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState("home");
-  const aboveMediumScreens = useMediaQuery("(min-width:1060px)")
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+  const isDesktop = useMediaQuery("(min-width: 1060px)");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage("home");
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className="App bg-deep-blue">
-     <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+    <div className="app bg-deep-blue">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <div className="w-5/6 mx-auto md:h-full">
+        {isDesktop && (
+          <DotGroup
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+          />
+        )}
+      </div>
     </div>
   );
 }
